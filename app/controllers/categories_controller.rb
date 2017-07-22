df2622b39b8250c7a77674f3f5a13ab0a2303b46
Antonio Franceskin <antonio.franceskin@gmail.com>
2017-07-22 15:26:24 -0400

    Actualizacion de develop de antonio

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,7 @@ class CategoriesController < ApplicationController
   # GET /categories/new
   def new
     @category = Category.new
+    puts "Pase por el  new"
   end
 
   # GET /categories/1/edit
