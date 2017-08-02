6a27bc72f1da3844a3af40efa2bee66ba5af8c5f
Antonio Franceskin <antonio.franceskin@gmail.com>
2017-08-02 07:45:07 -0400

    Mejorar busquedas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,89 +10,68 @@ class WelcomeController < ApplicationController
     manana = Date.today + 1
     semana = Date.today + 7
     mes = hoy.next_month 
+      @categories_carousel = Category.where("status= ? AND priority= ? 
+    AND avatar1_file_name <> ? AND avatar2_file_name <> ? 
+    AND avatar3_file_name <> ? AND start_date >= ?", 
+    true, 10, '', '', '',hoy).order("priority DESC, start_date ASC").reverse
     case params[:buscar]
         when "dia" then
-              puts "Pase por dia"
-              @categories_carousel = Category.where("status= ? AND priority= ? 
-                AND avatar1_file_name <> ? AND avatar2_file_name <> ? 
-                AND avatar3_file_name <> ?", 
-                true, 10, '', '', '')
               @categories_evento = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Evento', hoy).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Evento', hoy).order("priority DESC")
               @categories_foro = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Foro', hoy).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Foro', hoy).order("priority DESC")
               @categories_curso = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Curso', hoy).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Curso', hoy).order("priority DESC")
               @categories_taller = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Taller', hoy).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Taller', hoy).order("priority DESC")
               @categories_documental = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Documental', hoy).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Documental', hoy).order("priority DESC")
               @categories_otros = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Otro', hoy).order(:priority).reverse
-              @categories = Category.where("status = true AND start_date = ?", hoy).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Otro', hoy).order("priority DESC")
+              @categories = Category.where("status = true AND start_date = ?", hoy).order("priority DESC")
         when "manana" then
-              puts "Pase por manana"
-              @categories_carousel = Category.where("status= ? AND priority= ? 
-                AND avatar1_file_name <> ? AND avatar2_file_name <> ? 
-                AND avatar3_file_name <> ?", 
-                true, 10, '', '', '')
               @categories_evento = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Evento', manana).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Evento', manana).order("priority DESC")
               @categories_foro = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Foro', manana).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Foro', manana).order("priority DESC")
               @categories_curso = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Curso', manana).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Curso', manana).order("priority DESC")
               @categories_taller = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Taller', manana).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Taller', manana).order("priority DESC")
               @categories_documental = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Documental', manana).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Documental', manana).order("priority DESC")
               @categories_otros = Category.where("status= ?  
-                AND category_type = ? AND start_date = ?", true, 'Otro', manana).order(:priority).reverse
-              @categories = Category.where("status = true AND start_date = ?", manana).order(:priority).reverse
+                AND category_type = ? AND start_date = ?", true, 'Otro', manana).order("priority DESC")
+              @categories = Category.where("status = true AND start_date = ?", manana).order("priority DESC")
         when "semana" then
-              puts "Pase por semana"
-              @categories_carousel = Category.where("status= ? AND priority= ? 
-                AND avatar1_file_name <> ? AND avatar2_file_name <> ? 
-                AND avatar3_file_name <> ?", 
-                true, 10, '', '', '')
               @categories_evento = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Evento',hoy, semana).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Evento',hoy, semana).order("priority DESC")
               @categories_foro = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Foro',hoy, semana).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Foro',hoy, semana).order("priority DESC")
               @categories_curso = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Curso',hoy, semana).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Curso',hoy, semana).order("priority DESC")
               @categories_taller = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Taller',hoy, semana).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Taller',hoy, semana).order("priority DESC")
               @categories_documental = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Documental',hoy, semana).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Documental',hoy, semana).order("priority DESC")
               @categories_otros = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Otro',hoy, semana).order(:priority).reverse
-              @categories = Category.where("status = ? AND start_date BETWEEN ? AND ?" ,true,  hoy, semana).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Otro',hoy, semana).order("priority DESC")
+              @categories = Category.where("status = ? AND start_date BETWEEN ? AND ?" ,true,  hoy, semana).order("priority DESC")
         when "mes" then
-              puts "Pase por mes"
-              @categories_carousel = Category.where("status= ? AND priority= ? 
-                AND avatar1_file_name <> ? AND avatar2_file_name <> ? 
-                AND avatar3_file_name <> ?", 
-                true, 10, '', '', '')
               @categories_evento = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Evento',hoy, mes).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Evento',hoy, mes).order("priority DESC")
               @categories_foro = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Foro',hoy, mes).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Foro',hoy, mes).order("priority DESC")
               @categories_curso = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Curso',hoy, mes).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Curso',hoy, mes).order("priority DESC")
               @categories_taller = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Taller',hoy, mes).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Taller',hoy, mes).order("priority DESC")
               @categories_documental = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Documental',hoy, mes).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Documental',hoy, mes).order("priority DESC")
               @categories_otros = Category.where("status= ?  
-                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Otro',hoy, mes).order(:priority).reverse
-              @categories = Category.where("status = ? AND start_date BETWEEN ? AND ?" ,true,  hoy, mes).order(:priority).reverse
+                AND category_type = ? AND start_date BETWEEN ? AND ?", true, 'Otro',hoy, mes).order("priority DESC")
+              @categories = Category.where("status = ? AND start_date BETWEEN ? AND ?" ,true,  hoy, mes).order("priority DESC")
         else
-            puts "Pase por else"
-            @categories_carousel = Category.where("status= ? AND priority= ? 
-              AND avatar1_file_name <> ? AND avatar2_file_name <> ? 
-              AND avatar3_file_name <> ? AND start_date >= ?", 
-              true, 10, '', '', '',hoy).order("priority DESC, start_date ASC")
             @categories_evento = Category.where("status= ?  
               AND category_type = ? AND start_date >= ?", true, 'Evento', hoy).order("priority DESC, start_date ASC")
             @categories_foro = Category.where("status= ?  
